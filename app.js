831111a2d0ec6d62d4a63ea5d1f283324fe59020
Aman Jadon <jadonaman471@gmail.com>
2025-05-24 13:48:29 +0530

    removed comment

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use('/api/v1', urlRoutes);
 
-// Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
